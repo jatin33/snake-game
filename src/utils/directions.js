0a2dc286d48389b7d6176367300ebbb681a8818f
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-10-17 17:44:33 +0530

    Added basic game objects

---
+++ src/utils/directions.js
@@ -0,0 +1,8 @@
+const UP = 'ArrowUp';
+const DOWN = 'ArrowDown';
+const LEFT = 'ArrowLeft';
+const RIGHT = 'ArrowRight';
+
+// event.key to fetch value from keyboard
+
+export { UP, DOWN, LEFT, RIGHT };
---
+++ src/utils/gems.js
@@ -0,0 +1,17 @@
+export const gems = {
+    blue: {
+        name:'Silver',
+        color: 'blue',
+        points: 10
+    },
+    red: {
+        name:'Gold',
+        color: 'red',
+        points: 20
+    },
+    green: {
+        name:'Diamond',
+        color: 'green',
+        points: 30
+    }
+}
